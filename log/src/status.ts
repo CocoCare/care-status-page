61a1c402cc6db4ae8a594f27bdf79a04878e7cf9
Xingguo Sun <xingguo@cocoare.io>
2026-04-30 12:21:38 +0800

    fix: update validations

--- log/src/status.ts
+++ log/src/status.ts
@@ -10,9 +10,12 @@ export class StatusChecker {
     async verifyEndpoint(): Promise<boolean> {
         try {
             this.logger.info(`Contacting ${this.siteName}`);
-            const request = await axios.get(this.healthEndpoint);
+            const request = await axios.get(this.healthEndpoint, {
+                maxRedirects: 5,
+                validateStatus: (status: number) => status >= 200 && status < 400
+            });
             this.logger.debug(`Contacted ${this.healthEndpoint} with status ${request.status}`);
-            return request.status >= 200 && request.status < 300;
+            return request.status >= 200 && request.status < 400;
         } catch (err: unknown | Error | AxiosError) {
             if (axios.isAxiosError(err)) {
                 const axErr = err as AxiosError;
